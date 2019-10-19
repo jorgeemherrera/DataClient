5feb26cde103998936009a8d93ba8b4ee32cba2c
Jorge Eliecer <jorge.m.h@outlook.com>
2019-10-19 18:09:47 -0500

    JWT validation

--- node-rest-api/app.js
+++ node-rest-api/app.js
@@ -12,6 +12,8 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/users');
+
 
 mongoose.connect('mongodb+srv://jorgeHerrera:' + process.env.MONGO_ATLAS_PW +           '@node-rest-api-zl10v.mongodb.net/test?retryWrites=true&w=majority',
 {
@@ -41,6 +43,8 @@ app.use(bodyParser.json());
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/users', userRoutes);
+
 
 app.use((req, res, next) => {
     const error = new Error('Not found');

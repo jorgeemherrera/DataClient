d97f58d32613632ba346c51398211b48ce160221
Jorge Eliecer <jorge.m.h@outlook.com>
2019-10-20 12:36:59 -0500

    JWt protection

--- node-rest-api/api/routes/orders.js
+++ node-rest-api/api/routes/orders.js
@@ -7,6 +7,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const checkAuth = require('../middleware/check-auth');
 
 const Order = require('../models/order');
 const Product = require('../models/product');
@@ -14,7 +15,7 @@ const Product = require('../models/product');
 /**
  * Handle incoming GET request to /orders
  */
-router.get('/', (req, res, next) => {
+router.get('/', checkAuth, (req, res, next) => {
     Order.find()
     .select('product quantity _id')
     .populate('product', 'name')
@@ -46,7 +47,7 @@ router.get('/', (req, res, next) => {
 /**
  * Handle incoming POST request to /orders
  */
-router.post('/', (req, res, next) => {
+router.post('/', checkAuth, checkAuth, (req, res, next) => {
     Product.findById(req.body.productId)
         .then(product => {
             if(!product) {
@@ -85,7 +86,7 @@ router.post('/', (req, res, next) => {
 /**
  * Handle incoming GET ID request to /orders
  */
-router.get('/:orderId', (req, res, next) => {
+router.get('/:orderId', checkAuth, (req, res, next) => {
     Order.findById(req.params.orderId)
     .populate('product')
     .exec()
@@ -113,7 +114,7 @@ router.get('/:orderId', (req, res, next) => {
 /**
  * Handle incoming DELETE request to /orders
  */
-router.delete('/:orderId', (req, res, next) => {
+router.delete('/:orderId', checkAuth, (req, res, next) => {
     Order.remove({_id: req.params.orderId})
     .exec()
     .then(result => {
--- node-rest-api/api/routes/products.js
+++ node-rest-api/api/routes/products.js
@@ -8,6 +8,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const multer = require('multer');
+const checkAuth = require('../middleware/check-auth');
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
@@ -72,7 +73,7 @@ router.get('/', (req, res, next) => {
     })
 });
 
-router.post('/', upload.single('productImage'), (req, res, next) => {
+router.post('/', checkAuth, upload.single('productImage'), (req, res, next) => {
     /**
      * Data from MongoDB model Schema
      */
@@ -105,7 +106,7 @@ router.post('/', upload.single('productImage'), (req, res, next) => {
     });
 });
 
-router.get('/:productId' , (req, res, next) =>{
+router.get('/:productId', (req, res, next) =>{
     const id = req.params.productId;
     Product.findById(id)
     .select('name price _id productImage')
@@ -130,7 +131,7 @@ router.get('/:productId' , (req, res, next) =>{
     });
 });
 
-router.patch('/:productId' , (req, res, next) =>{
+router.patch('/:productId', checkAuth, (req, res, next) =>{
     const id = req.params.productId;
     const updateOps = {};
     for(const ops of req.body) {
@@ -154,7 +155,7 @@ router.patch('/:productId' , (req, res, next) =>{
         });
 });
 
-router.delete('/:productId' , (req, res, next) =>{
+router.delete('/:productId', checkAuth, (req, res, next) =>{
     const id = req.params.productId;
     Product.remove({_id: id})
     .exec()
